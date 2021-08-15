89d094024db117cda844e47e8adc25f7c0a9dd4b
Taylor Chou <ychou200091@gmail.com>
2021-08-15 17:39:28 +0800

    new

--- scripy.py
+++ scripy.py
@@ -5,6 +5,6 @@ print(sys.version)
 print(sys.executable)
 
 r = requests.get("https://google.com")
-if __name__ == "__main__":
-    print(r.status_code)
-    print("HELLO~~~~")
+
+print(r.status_code)
+print(r.ok)
